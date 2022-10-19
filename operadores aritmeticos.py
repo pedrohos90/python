021e3086eb74e56215bae405800e0a9af7d0a6f4
pedrohos90 <pedrohos_@hotmail.com>
2022-10-19 18:46:07 -0300

    Adicionei mais arquivos e exercicios ao repositorio Python

---
+++ operadores aritmeticos.py
@@ -0,0 +1,17 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)    #python segue a precedencia padrao da matematica
+print(produto_1 * produto_2)     #caso queira mudar a precedencia, deve adicionar os
+print(produto_1 ** produto_2)    #parenteses na operacao
+print(produto_1 % produto_2)
+
+x = (10 + 5) * 4
+y = (10 / 2) + 25 * ((2 - 2) ** 2)
+print(x)
+print(y)
+
+
---
+++ operadores associacao.py
@@ -0,0 +1,9 @@
+frutas = ["uva", "kiwi", "Laranja"] # letras maiusculas ou minusculas tem diferenca
+curso = "Curso de Python"
+
+
+print("laranja" in frutas)
+print("limao" not in frutas) # NOT IN significa nao esta
+print("Python" in curso)
+
+
